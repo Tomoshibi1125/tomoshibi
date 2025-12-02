6b53db90765a5162e60f229c480a0bb10c37a936
Tomoshibi1125 <jjcaldwelljr@gmail.com>
2025-12-02 15:52:34 -0800

    Add files via upload

---
+++ solo-system-app-v0.3.2/public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  clients.claim();
+});
+
+self.addEventListener("fetch", () => {
+  // network-first; caching can be added later
+});
